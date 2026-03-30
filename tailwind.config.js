20374f9191ba2726727838c0c57d871ce9bb4643
Aymane <carlejohnsonbus@gmail.com>
2026-03-30 18:25:18 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: [
+    "./**/*.html",
+    "./**/*.js"
+  ],
+  theme: {
+    extend: {
+        colors : {
+          "primary" : "#b41f17",
+          "secondary" : "#2f312f",
+        }
+    },
+  },
+  plugins: [],
+}
