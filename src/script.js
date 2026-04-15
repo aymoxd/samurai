519ebd103fac3bd910e5eb1c9e835a70dea0e2ef
Aymane <carlejohnsonbus@gmail.com>
2026-04-15 15:37:09 +0100

    add whatsapp button , change some content , add aside bar

--- src/script.js
+++ src/script.js
@@ -1,25 +1,50 @@
+const openMenuBtn = document.getElementById("openMenuBtn");
+const closeMenuBtn = document.getElementById("closeMenuBtn");
+const overlay = document.querySelector(".overlay");
+const aside = document.getElementById("aside");
+
+//===================== menu "asidebar" ===========================
+function openMenu() {
+  aside.classList.remove("-right-72");
+  aside.classList.add("right-0");
+  overlay.classList.remove("hidden");
+}
+function closeMenu() {
+  aside.classList.remove("right-0");
+  aside.classList.add("-right-72");
+  overlay.classList.add("hidden");
+}
+
+openMenuBtn.addEventListener("click", openMenu);
+closeMenuBtn.addEventListener("click", closeMenu);
+overlay.addEventListener("click", closeMenu);
+
+// Close menu when clicking on a link
+const menuLinks = document.querySelectorAll("#aside a");
+menuLinks.forEach((link) => {
+  link.addEventListener("click", closeMenu);
+});
 
 //===================== GSAP ===========================
 gsap.registerPlugin(ScrollTrigger);
-gsap.utils.toArray(".box").forEach(box => {
-    gsap.from(box, {
-        y: 40,
-        opacity: 0,
-        duration: 0.35,
-        ease: "power3.out",
-        scrollTrigger: {
-            trigger: box,
-            start: "top bottom",
-            toggleActions: "play none none none",
-            once: true
-        }
-    });
+gsap.utils.toArray(".box").forEach((box) => {
+  gsap.from(box, {
+    y: 40,
+    opacity: 0,
+    duration: 0.35,
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: box,
+      start: "top bottom",
+      toggleActions: "play none none none",
+      once: true,
+    },
+  });
 });
-
 
 //=================== LENIS =============================
 const lenis = new Lenis({
-  duration: 2.0, 
+  duration: 2.0,
   smooth: true,
   smoothTouch: false,
 });
@@ -32,7 +57,7 @@ function raf(time) {
 requestAnimationFrame(raf);
 
 //=================== SWIIPER =============================
-
+/*
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   loop: true,
@@ -65,3 +90,4 @@ const swiper = new Swiper('.swiper', {
   },
 
 });
+*/
